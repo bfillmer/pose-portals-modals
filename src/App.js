2e8f3c544b4f83ab86b533ba6f6f3c361e0ec20f
Bryan Fillmer <bryan.fillmer@gmail.com>
2017-07-15 05:42:09 -0600

    Refactor css with styled-components.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
-import React, { Component } from 'react'
-import logo from './logo.svg'
-import './App.css'
 
-class App extends Component {
-  render () {
-    return (
-      <div className='App'>
-        <div className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className='App-intro'>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    )
-  }
-}
+import React from 'react'
+import styled, {keyframes} from 'styled-components'
 
-export default App
+import logo from 'logo.svg'
+
+const Wrapper = styled.div`
+  text-align: center;
+`
+
+const Header = styled.header`
+  background-color: #222;
+  height: 150px;
+  padding: 20px;
+  color: white;
+`
+
+const logoSpin = keyframes`
+  from { transform: rotate(0deg); }
+  to { transform: rotate(360deg); }
+`
+
+const Logo = styled.img`
+  animation: ${logoSpin} infinite 20s linear;
+  height: 80px;
+`
+
+const Intro = styled.p`
+  font-size: large;
+`
+
+export const App = () => (
+  <Wrapper>
+    <Header>
+      <Logo src={logo} alt='logo' />
+      <h2>Welcome to React</h2>
+    </Header>
+    <Intro>
+      To get started, edit <code>src/App.js</code> and save to reload.
+    </Intro>
+  </Wrapper>
+)
