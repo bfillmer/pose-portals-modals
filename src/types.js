ba5818a1e91f1d191108809efdb053ec9ca72c5b
Bryan Fillmer <bryan.fillmer@gmail.com>
2017-07-16 08:49:36 -0600

    Updates.

--- src/types.js
+++ src/types.js
@@ -5,8 +5,5 @@ export const ABOUT = 'route/ABOUT'
 
 // MESSAGES
 export const ADDED_MESSAGE = 'messages/ADDED_MESSAGE'
-export const REMOVED_MESSAGE = 'messages/REMOVED_MESSAGE'
-
-// TASKS
-export const ADDED_TASK = 'tasks/ADDED_TASK'
+export const REMOVED_MESSAGE = 'tasks/ADDED_TASK'
 export const REMOVED_TASK = 'tasks/REMOVED_TASK'
