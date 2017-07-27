5fdc54b391511d9d531d3d30c2b94d3e17fc4240
Bryan Fillmer <bryan.fillmer@gmail.com>
2017-07-26 18:56:50 -0600

    Fiddle with loader, testing now deployments.

--- src/view/Shared/OverlayLoader.js
+++ src/view/Shared/OverlayLoader.js
@@ -5,7 +5,7 @@ import styled from 'styled-components'
 
 import {OVERLAY_LOADER} from 'types'
 
-const Overlay = styled.div`
+const Scrim = styled.div`
   z-index: 999;
   position: absolute;
   top: 0;
@@ -14,6 +14,16 @@ const Overlay = styled.div`
   left: 0;
   background-color: ${({theme}) => theme.grays.dark};
 `
+
+const H1 = styled.h1`
+  color: ${({theme}) => theme.white};
+`
+
+const Overlay = () => (
+  <Scrim>
+    <H1>Loading</H1>
+  </Scrim>
+)
 
 const mapStateToProps = state => ({
   show: state.interactions.includes(OVERLAY_LOADER)
