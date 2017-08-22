09e3c971f105a829600e5a5488574b5a0dc6e7cb
Bryan Fillmer <bryan.fillmer@gmail.com>
2017-08-22 11:10:03 -0600

    Global style tweak, library upgrades.

--- src/view/theme.js
+++ src/view/theme.js
@@ -1,4 +1,7 @@
 
+import {injectGlobal} from 'styled-components'
+
+// Reusable definitions for colors, spacings, etc.
 export const theme = {
   white: '#f4f4f4',
   grays: {
@@ -8,6 +11,16 @@ export const theme = {
   },
   bottomMargin: '1.5rem'
 }
+
+// Inject some global styles that are most likely to be coupled to theme variables.
+injectGlobal`
+  body {
+    font-size: 16px;
+    font-weight: normal;
+    font-family: sans-serif;
+    background-color: #fff;
+  }
+`
 
 // Simple helper function, takes in any number of props mapping to properties within the theme
 // object and returns the value.
