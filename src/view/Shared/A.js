eafbc21134c659cf3071d930006437eaf1503c96
Bryan Fillmer <bryan.fillmer@gmail.com>
2018-04-22 14:34:07 -0600

    More robust and faster A component.

--- src/view/Shared/A.js
+++ src/view/Shared/A.js
@@ -2,31 +2,29 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
-import {theme} from 'styled-system'
 
 import {Text} from 'view/Shared/Primitives'
 
-import {history} from 'router'
+import {history, router} from 'router'
 
 // Wrap hickory's navigate function for our Link component.
-const makeLinkAction = href => e => {
+const makeLinkAction = pathname => e => {
   e.preventDefault()
-  history.navigate(href)
+  history.navigate(pathname)
 }
 
 // Usage of the external prop allows for standard a tag behavior. This allows for usage of Link
 // component for all linking needs, keeping consistent styling.
-const Link = ({children, external, href, ...props}) => external
-  ? (<Text is='a' href={href} {...props}>{children}</Text>)
-  : (<Text is='a' href={href} onClick={makeLinkAction(href)} {...props}>{children}</Text>)
+function Link ({children, external, href, ...props}) {
+  if (external) {
+    return (<Text is='a' href={href} {...props}>{children}</Text>)
+  }
+  const pathname = router.addons.pathname(href)
+  return (<Text is='a' href={pathname} onClick={makeLinkAction(pathname)} {...props}>{children}</Text>)
+}
 
 // Export as a styled component for ease of use with the .extend method.
-export const A = styled(Link)`
-  color: ${theme('primary.base')};
-  &:hover {
-    color: ${theme('primary.dark')};
-  }
-`
+export const A = styled(Link)``
 
 A.propTypes = {
   href: PropTypes.string.isRequired,
